28b58917f91b2ce90d309796b1aed9f47b841293
Pha <anhphamhoang04@gmail.com>
2024-10-05 18:49:36 +0700

    10/26

--- HUSTask/11linkedlistmanipulation.cpp
+++ HUSTask/11linkedlistmanipulation.cpp
@@ -6,7 +6,17 @@ struct Node{
     Node* next;
 };
 
-Node* addfirst(Node* head,int value){ //take head pointer, return new head pointer
+bool checkint(Node* head, int check){ //checked
+    Node* temp = head;
+    while (temp != nullptr){
+        if (temp->num == check)
+            return true;
+        temp = temp->next;
+    }
+    return false;
+}
+
+Node* addfirst(Node* head,int value){ //take head pointer, return new head pointer //checked
     Node* new_node = new Node();
     new_node->num = value;
     new_node->next = head;
@@ -14,10 +24,81 @@ Node* addfirst(Node* head,int value){ //take head pointer, return new head point
     return head;
 }
 
+Node* addlast(Node* head,int value){ //checked
+    Node* new_node = new Node();
+    new_node->num = value;
+    new_node->next = nullptr;
+    if (head == nullptr) { //head ini alway be null
+        return new_node;
+    }
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+    return head;
+}
+
+Node* remove(Node* head,int value){ // checked
+    Node* temp = head;
+    if (temp->num==value){
+        return temp->next;
+    }
+    while (temp->next->num != value){
+        temp = temp->next;
+    }
+
+    temp->next = temp->next->next;
+    return head;
+}
+
+Node* addafter(Node* head,int value1,int value2){ //find value2, add value1 after 
+    Node* temp = head;
+    while (temp->num != value2){
+        temp = temp->next;
+    }
+
+    Node* new_node = new Node();
+    new_node->next=temp->next;
+    temp->next = new_node;
+    new_node->num=value1;
+    return head;
+}
+
+Node* addbefore(Node* head,int value1,int value2){ //find value2, add value1 before 
+    Node* temp = head;
+    while (temp->next->num != value2){
+        temp = temp->next;
+    }
+
+    Node* new_node = new Node();
+    new_node->next=temp->next;
+    temp->next = new_node;
+    new_node->num=value1;
+    return head;
+}
+
+Node* reversee(Node* head){ //checked
+    Node* temp = head;
+    Node* next;
+    Node* pre;
+    next = nullptr;
+    pre = nullptr;
+
+    while (temp != nullptr){
+        next = temp->next;
+        temp->next = pre;
+        pre = temp;
+        temp = next;
+    }
+    head = pre;
+    return head;
+}
+
 void result(Node* head){ // print result
     Node* temp = head;
     while (temp != nullptr){
-        cout << temp->num;
+        cout << temp->num <<" ";
         temp = temp->next;
     }
 }
@@ -30,10 +111,50 @@ int main(){
         cin >> arr[i];
     }
 
-    Node* head;
-    for (int i=n-1 ; i>=0 ; i--){
-        head=addfirst(head,arr[i]); // head is pointer
+    Node* head=nullptr;
+    for(int i=0 ; i<n ; i++){
+        head=addlast(head,arr[i]);
     }
-    result(head);
 
+    string sinput="";
+    int iinput;
+    int iiinput;
+    while(sinput != "#"){
+        cin >> sinput;
+        if (sinput == "#"){
+            result(head);
+            break;
+        } 
+        else if (sinput=="reverse"){
+            head=reversee(head);
+        }
+        else{
+            cin >> iinput;
+            if (sinput=="addfirst"){ // check iinput existance, if no then excute
+                if (checkint(head,iinput)==false)
+                    head=addfirst(head,iinput);
+            }
+            else if (sinput=="addlast"){ // check iinput existance, if no then excute
+                if (checkint(head,iinput)==false)
+                    head=addlast(head,iinput);
+            }
+            else if (sinput=="remove"){ // check iinput existance, if yes then excute
+                if (checkint(head,iinput)==true)
+                    head=remove(head,iinput);
+            }
+            else{
+                cin >> iiinput;
+                if (sinput=="addafter"){ // check existance, if iinput no-iiinput yes then excute
+                    if (checkint(head,iinput)==false && checkint(head,iiinput)==true)
+                        head=addafter(head,iinput,iiinput);
+                }
+                else if (sinput=="addbefore"){ // check existance, if iinput no-iiinput yes then excute
+                    if (checkint(head,iinput)==false && checkint(head,iiinput)==true)
+                        head=addbefore(head,iinput,iiinput);
+                }
+            }
+        }
+        sinput = "";
+    }
+    return 0;
 }
---
+++ test1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> // test while
+using namespace std;
+
+int main(){
+    string a="";
+    while (a[0]!='#'){
+        cin>>a;
+    }
+}
