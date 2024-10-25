11edce95329c18027c5f046828e3b3c42fd89439
Pha <anhphamhoang04@gmail.com>
2024-10-25 09:34:43 +0700

    adjust

--- HUSTask/15hashinteger.cpp
+++ HUSTask/15hashinteger.cpp
@@ -2,4 +2,4 @@
 #include <unordered_map>
 using namespace std; unordered_map <string, int> a;
 
-int main(){string b="";string c="";while(b!="*"){cin>>b;a[b]=1;}while(c!="***"){cin>>c>>b;if(c=="find"){cout<<a[b]<< endl;}else if(c =="insert"){cout<<abs(a[b]-1)<<endl;a[b]=1;}}return 0;}
+int main(){string b="";string c="";while(b!="*"){cin>>b;a[b]=1;}while(c!="***"){cin>>c>>b;if(c=="find"){cout<<a[b]<<endl;}else if(c =="insert"){cout<<abs(a[b]-1)<<endl;a[b]=1;}}return 0;}
